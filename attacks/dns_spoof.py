a0da9d1d8c2aa5dc8b59b00504b9a6dcd932fb75
JulienErblandEPFL <julien.erbland@epfl.ch>
2025-05-15 18:35:38 +0200

    start of dns spoofing

--- attacks/dns_spoof.py
+++ attacks/dns_spoof.py
@@ -1,7 +1,14 @@
+from scapy.layers.dns import DNS, DNSQR
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+from scapy.sendrecv import sendp
 
+def spoof_dns(pkt, spoofed_domains, fake_ip, attacker_mac, iface):
+    if DNS in pkt and DNSQR in pkt:
+        #Extract the queried domain and remove trailing dot(e.g., 'facebook.com.')
+        queried = pkt[DNSQR].qname.decode().strip('.')
 
-def spoof_dns(pkt, spoofed_domains, fake_ip):
-    #pkt : captured packet
-    #spoofed_domains : domain we want to poison
-    #fake_ip : fake IP address to return in the DNS reply
+        if queried in spoofed_domains:
+            print(f"Spoofing DNS response for {queried}")
+            # TODO: build and send the DNS reply using attacker_mac and fake_ip
     return
